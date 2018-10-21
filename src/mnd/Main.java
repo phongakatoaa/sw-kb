a991e655ff696036d3de9ce21aa2b7c5f2a5fff9
Nguyen Duc Manh <manh16nd@gmail.com>
2018-10-21 22:07:49 +0700

    init

---
+++ src/mnd/Main.java
@@ -0,0 +1,31 @@
+package mnd;
+import vn.pipeline.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // "wseg", "pos", "ner", and "parse" refer to as word segmentation, POS tagging, NER and dependency parsing, respectively.
+        String[] annotators = {"wseg", "pos", "ner"};
+        VnCoreNLP pipeline = new VnCoreNLP(annotators);
+
+        String str = "Bùi Anh Tuấn sinh vào ngày 9 tháng 9 năm 1991 ở tại Hà Nội, anh đã có một khuôn mặt lãng tử và có một giọng hát cao, thanh.\n";
+
+        Annotation annotation = new Annotation(str);
+        pipeline.annotate(annotation);
+
+        System.out.println(annotation.toString());
+        // 1    Ông                 Nc  O       4   sub
+        // 2    Nguyễn_Khắc_Chúc    Np  B-PER   1   nmod
+        // 3    đang                R   O       4   adv
+        // 4    làm_việc            V   O       0   root
+        // ...
+
+        //Write to file
+        PrintStream outputPrinter = new PrintStream("output.txt");
+        pipeline.printToFile(annotation, outputPrinter);
+
+        // You can also get a single sentence to analyze individually
+        Sentence firstSentence = annotation.getSentences().get(0);
+        System.out.println(firstSentence.toString());
+    }
+}
